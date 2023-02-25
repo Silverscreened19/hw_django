332c59b48252e2cdb4848a970a2f1a1fc53049da
Olya Tatianina <olya.tatianina@gmail.com>
2023-02-25 12:14:38 +0400

    second task

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -8,11 +8,25 @@ from django.shortcuts import render
 # так как при перезапуске приложения они обнулятся
 counter_show = Counter()
 counter_click = Counter()
+counter_show['original'] = 0
+counter_show['test'] = 0
+counter_click['original'] = 0
+counter_click['test'] = 0
 
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
-    return render(request, 'index.html')
+    from_landing = request.GET.get('from-landing')
+    if from_landing == 'original':
+        counter_click['original'] += 1
+        # print(f'кол-во переходов {counter_click}')
+        return render(request, 'index.html')
+    elif from_landing == 'test':
+        counter_click['test'] += 1
+        # print(f'кол-во переходов {counter_click}')
+        return render(request, 'index.html')
+    else:
+        return render(request, 'index.html')
 
 
 def landing(request):
@@ -20,13 +34,39 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
-    return render(request, 'landing.html')
+    ab_test_arg = request.GET.get('ab-test-arg')
+    if ab_test_arg == 'original':
+        counter_show['original'] += 1
+        # print(counter_show['original'])
+        # print(f'количество ПОКАЗОВ {counter_show}')
+        return render(request, 'landing.html')
+    elif ab_test_arg == 'test':
+        counter_show['test'] += 1
+        # print(counter_show['test'])
+        # print(f'кол-во ПОКАЗОВ {counter_show}')
+        return render(request, 'landing_alternate.html')
 
 
 def stats(request):
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Для вывода результат передайте в следующем формате:
+    try:
+        test_conversion = round(
+            counter_click['test'] / counter_show['test'], 1)
+    except ZeroDivisionError:
+        test_conversion = 0
+    try:
+        original_conversion = round(
+            counter_click['original'] / counter_show['original'], 1)
+    except ZeroDivisionError:
+        original_conversion = 0
+    print(f'статистика тестового лэндинга {test_conversion}\n'
+          f'статистика базового лэндинга {original_conversion}')
     return render(request, 'stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': test_conversion,
+        'original_conversion': original_conversion,
     })
+
+
+# http://127.0.0.1:8000/landing/?ab-test-arg=test
+# http://127.0.0.1:8000/landing/?ab-test-arg=original
--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, redirect
+import csv
+from pprint import pprint
+from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
 from django.urls import reverse
+import urllib.parse
 
 
 def index(request):
@@ -7,13 +11,37 @@ def index(request):
 
 
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
-    return render(request, 'index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'},
-                         {'Name': 'другое название', 'Street': 'другая улица', 'District': 'другой район'}],
-        'current_page': current_page,
-        'prev_page_url': None,
-        'next_page_url': next_page_url,
-    })
+    bus_stops_list = []
+    with open('pagination/data-398-2018-08-30.csv', newline='', encoding='cp1251') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            stop_line = {
+                'Name': row['Name'], 'Street': row['Street'], 'District': row['District']}
+            bus_stops_list.append(stop_line)
 
+    page_number = int(request.GET.get('page', 1))
+    paginator = Paginator(bus_stops_list, 10)
+    current_page = paginator.get_page(page_number)
+    objects_on_page = current_page.object_list
+    next_page_url = f'{reverse(bus_stations)}?{urllib.parse.urlencode({"page": current_page})}'
+
+    if current_page.has_next():
+        next_page_params = {'page': current_page.next_page_number()}
+        params = urllib.parse.urlencode(next_page_params)
+        next_page_url = f'{reverse(bus_stations)}?{params}'
+
+    prev_page_url = f'{reverse(bus_stations)}?{urllib.parse.urlencode({"page": current_page})}'
+
+    if current_page.has_previous():
+        prev_page_params = {'page': current_page.previous_page_number()}
+        params = urllib.parse.urlencode(prev_page_params)
+        prev_page_url = f'{reverse(bus_stations)}?{params}'
+
+    context = {
+        'bus_stations': objects_on_page,
+        'current_page': current_page,
+        'prev_page_url': prev_page_url,
+        'next_page_url': next_page_url,
+    }
+
+    return render(request, 'index.html', context)
